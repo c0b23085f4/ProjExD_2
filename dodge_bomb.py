724c5e76d20f716693f92192779efeac01c27730
takeru <c0b23085f4@edu.teu.ac.jp>
2024-10-01 14:33:02 +0900

    爆弾の作成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,9 +1,16 @@
 import os
+import random
 import sys
 import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
+DELTA = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+    }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -14,15 +21,15 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20, 20))  # 空のSurface
+    bb_img.set_colorkey((0, 0, 0))  # 爆弾の四隅を透過させる
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_rct = bb_img.get_rect()  # 爆弾Rectの抽出
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5  # 爆弾の速度
     clock = pg.time.Clock()
     tmr = 0
-    DELTA = {
-        pg.K_UP: (0, -5), 
-        pg.K_DOWN: (0, +5),
-        pg.K_LEFT: (-5, 0), 
-        pg.K_RIGHT: (+5, 0)
-        }
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -30,7 +37,7 @@ def main():
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0]
+        sum_mv = [0, 0]  # 横座標, 縦座標
         # if key_lst[pg.K_UP]:
         #     sum_mv[1] -= 5
         # if key_lst[pg.K_DOWN]:
@@ -41,11 +48,12 @@ def main():
         #     sum_mv[0] += 5
         for key, tpl in DELTA.items():
             if key_lst[key]:
-                sum_mv[0] += tpl[0]
-                sum_mv[1] += tpl[1]
-
+                sum_mv[0] += tpl[0]  # 横方向
+                sum_mv[1] += tpl[1]  # 縦方向
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
